95410e39438550224386f16ce3506b08fe079877
Gaurav1991 <gbgoswami9005@gmail.com>
2017-04-10 15:23:30 +1000

    Add files via upload

---
+++ Solution 1/Solution 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_1
+{
+    class ReadFromFile
+{
+    static void Main()
+    {
+        string text = System.IO.File.ReadAllText(@"H:demo.txt");
+
+        System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+           
+        string[] lines = System.IO.File.ReadAllLines(@"H:demo.txt");
+        System.Console.WriteLine();
+        System.Console.ReadKey();
+    }
+}
+}
---
+++ Solution2/Solution2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class ReadFromFile
+{
+    static void Main()
+    {
+        string text = System.IO.File.ReadAllText(@"H:demo.txt");
+
+        System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+        List<string> list = new List<string>();
+        using (StreamReader reader = new StreamReader(@"H:demo.txt"))
+        {
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                var parts = line.Split(',');
+                Array.Sort(parts);
+                // Array.Reverse(parts);
+                list.Add(string.Join(" ", parts));
+            }
+        }
+
+        list.Sort(new NameComparer());
+        using (StreamWriter writer = new StreamWriter("opfile.txt"))
+        {
+            foreach (var line in list)
+                writer.WriteLine(line);
+        }
+                System.Console.WriteLine("Press any key to exit.");
+        System.Console.ReadKey();
+    }
+    public class NameComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (x == null && y == null) return 0;
+
+            if (x == null || y == null) return -1;
+
+            var parts1 = x.Split();
+            var parts2 = y.Split();
+
+            if (parts1.Length > 1 && parts2.Length > 1)
+            {
+                if (parts1[0] != parts2[0]) return parts1[0].CompareTo(parts2[0]);
+
+                return parts1[1].CompareTo(parts2[1]);
+            }
+
+            return x.CompareTo(y);
+        }
+    }
+}
